cf8b5d5f06bafd811c96663427ade7754f8023e8
Sven Fuchs <svenfuchs@artweb-design.de>
2012-01-06 12:37:35 +0100

    style flashes, hamlfy the layout

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,3 @@
 class ProfilesController < ApplicationController
   before_filter :authenticate_user!, :only => :show
-
-  # def index
-  #   render json: User.with_user_packages #.by_package
-  # end
 end

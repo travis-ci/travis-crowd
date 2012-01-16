cdc01119a50120834559daba595f478026e18105
Sven Fuchs <svenfuchs@artweb-design.de>
2012-01-16 16:43:57 +0100

    add a postmark initializer

---
+++ config/initializers/postmark.rb
@@ -0,0 +1,6 @@
+settings = Rails.application.config.settings
+config   = Rails.application.config.action_mailer
+
+config.delivery_method   = :postmark
+config.postmark_settings = { :api_key => settings.postmark.api_key }
+

7f413ba5539ba0d9d72f79b23a408ab5ac398ace
Sven Fuchs <svenfuchs@artweb-design.de>
2012-01-17 10:48:36 +0100

    try using smtp as a delivery method because, the postmark method doesn't support inline attachments

--- config/initializers/postmark.rb
+++ config/initializers/postmark.rb
@@ -1,6 +1,14 @@
 if Rails.env.production?
   settings = Rails.application.config.settings
 
-  ActionMailer::Base.delivery_method   = :postmark
-  ActionMailer::Base.postmark_settings = { :api_key => settings.postmark.api_key }
+  # ActionMailer::Base.delivery_method   = :postmark
+  # ActionMailer::Base.postmark_settings = { :api_key => settings.postmark.api_key }
+
+  ActionMailer::Base.delivery_method   = :smtp
+  ActionMailer::Base.postmark_settings = {
+    address:   'smtp.postmarkapp.com',
+    user_name: settings.postmark.api_key,
+    password:  settings.postmark.api_key,
+    domain:    'love.travis-ci.org'
+  }
 end

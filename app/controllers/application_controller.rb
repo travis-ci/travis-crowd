18f4bb149956ffb1fff96c57629d8411822bce47
Sven Fuchs <svenfuchs@artweb-design.de>
2012-01-21 17:51:05 +0100

    protect production with http password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :protect_production
+
+  protected
+
+    def protect_production
+      if request.host == 'love.travis-ci.org'
+        authenticate_or_request_with_http_basic do |username, password|
+          username == 'travis' && password == 'crowd'
+        end
+      end
+    end
 end
 

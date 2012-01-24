0085f4df0ac948689c44cee38f6b3411c7d41815
Sven Fuchs <svenfuchs@artweb-design.de>
2012-01-25 00:39:39 +0100

    add script/followers.rb to determine the number unique followers between given accounts

---
+++ script/followers.rb
@@ -0,0 +1,14 @@
+require 'twitter'
+
+followers = %w(travisci joshkalderimis konstantinhaase svenfuchs).inject([]) do |ids, handle|
+  cursor = '-1'
+  while cursor != 0 do
+    followers = Twitter.follower_ids(handle, :cursor => cursor)
+    ids += followers.ids || []
+    cursor = followers.next_cursor
+    sleep(2)
+  end
+  ids
+end.uniq
+
+p followers.size

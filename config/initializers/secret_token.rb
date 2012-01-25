5d5c3ce733485550ec14e02485154c72e872f4b6
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-01-25 15:06:54 +0100

    the secret token we use on staging and prod needs to come from a config var, since this app is open to all

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Travis::Application.config.secret_token = '6e2b941713e5b9fa57cdff558772b88dc6dca42e74641bb01b835b0caa70526a0baf3c264a5bb6815cf1f5943ed48499ec66939e8984e0d84beed97b01923c0c'
+dev_secret_token = 'ceebd28ebf21ac31d55796902b5bf861e9a027580ca324387a0f2da71fb2f8fd9a896ada18b3398dc4bd464b9372e1f7d2057c7e1a10a24c42a03b7fcd577850'
+
+Travis::Application.config.secret_token = Rails.application.config.settings.secret_token || dev_secret_token

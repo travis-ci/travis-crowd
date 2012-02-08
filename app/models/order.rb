7f4af96e1d9afc0725a139b346c0fd6a28306d57
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-02-08 21:14:43 +0100

    always check total

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order < ActiveRecord::Base
 
   accepts_nested_attributes_for :user, :billing_address, :shipping_address
 
-  before_validation on: :create do
+  before_validation do
     self.total ||= package.price
   end
 

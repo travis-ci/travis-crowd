3791200de92ff8f60d2997c9372e7622c38515b2
Mathias Meyer <meyer@paperplanes.de>
2012-09-25 18:41:51 +0200

    Add a sender email for Devise.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,7 @@ Devise.setup do |c|
   require 'devise/orm/active_record'
 
   c.http_authenticatable = true
+  c.mailer_sender = 'support@travis-ci.org'
 
   # c.omniauth :github, settings.github.client_id, settings.github.client_secret, :scope => settings.github.scope
   # c.omniauth :twitter, settings.twitter.client_id, settings.twitter.client_secret
